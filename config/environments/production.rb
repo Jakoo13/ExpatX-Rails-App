99a44dc34a8ba957e1918c177ab876a52e34414b
Jacob Woodruff <jkwoodruff93@gmail.com>
2023-04-02 13:56:13 -0400

    updated name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "Netigo_Rails_App_production"
+  # config.active_job.queue_name_prefix = "ExpatX_Rails_App_production"
 
   config.action_mailer.perform_caching = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[index show]
+  
   devise_for :users,
   path: '',
   # This changes default path names for devise routes
